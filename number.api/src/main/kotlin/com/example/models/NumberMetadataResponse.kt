d2e8620ebd57f1ec0e5302789e01d56a73625946
Kagisho Tsoka <kmtsoka@gmail.com>
2022-12-12 09:13:36 +0200

    restructure project

--- number.api/src/main/kotlin/com/example/models/NumberMetadataResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.models
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-class NumberMetadataResponse {
-    var number : Int = 0
-    var nameOfDayOfTheWeek = DayOfTheWeekEnum.Unknown
-}
